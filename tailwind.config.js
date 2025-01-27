80d616cebbd85977299ba38fcbf458d14ae23e8c
Melthud <urquizotqm@gmail.com>
2025-01-27 10:09:44 -0500

    primer envio

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Ajusta según la ubicación de tus archivos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
